0ce3cb753763b7df0ee641e8f8be47b974391582
Chris Harrison <c.harrison1988@gmail.com>
2018-02-26 15:27:48 +0000

    Change new rota email text

--- src/Views/new-rota-email.blade.php
+++ src/Views/new-rota-email.blade.php
@@ -3,7 +3,7 @@
 
 <body>
 <p>Dear all,</p>
-<p>Below is next week's rota.</p>
+<p>Here are the dishwasher assignees for next week.</p>
 <table border="1" cellpadding="10">
     <thead>
         @foreach ($rota->getAssignedTimeSlots() as $timeSlot)
@@ -22,13 +22,13 @@
         </tr>
     </tbody>
 </table>
-<p>Instructions: When you are on duty, you are responsible for the following:</p>
+<p>Responsibilities when on duty:</p>
 <ol>
     <li>Empty the dishwasher in the morning (start it if it failed to start the night before).</li>
-    <li><b>Make sure the bins are emptied</b> if they are full (or approaching capacity). This should be checked throughout the day.</li>
+    <li>Make sure the bins are emptied if they are full (or approaching capacity). This should be checked throughout the day.</li>
     <li>Start the dishwasher before leaving the office at the end of the day.</li>
 </ol>
-<p>Everyone is responsible for loading their own dishes into the dishwasher throughout the day.</p>
+<p>Everyone is responsible for loading their own dishes into the dishwasher throughout the day. Please don't leave dishes in the sink or on the side.</p>
 <p>Regards,<br>Dishwasher bot</p>
 </body>
 </html>
